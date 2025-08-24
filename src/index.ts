20cadc8a102ccf6d10790289ad43cfa979ec4962
davidlkarlsson <david.karlsson@stud.sti.se>
2025-08-24 20:31:19 +0200

    lektion-2-webservice-hosting_ MongoDB: replaced old content for tsconfig.json for a dist folder, fix port index.ts and in .env file, fix render domain address, added files to .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express"
+import { env } from "node:process"
 
 const app = express()
-const port: number = 3000
+const port: number = Number(env.PORT) || 3000
+const address: string = "0.0.0.0" // Required for Render
 
 app.get("/", (request, response) => {
     response.send("Hello World!")
 })
 
-app.listen(port, () => {
+app.listen(port, address, () => {
 console.log(`Listening to port ${port}`)
 })
