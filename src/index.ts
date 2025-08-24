e46cb15b22108b03bd5a691708a4ac365f6e0ece
davidlkarlsson <david.karlsson@stud.sti.se>
2025-08-24 22:00:31 +0200

    lektion-2-webservice-hosting_ MongoDB: added interface User with an endpoint and response status

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,48 @@
-import express from "express"
-import { env } from "node:process"
+import express from "express";
+import { env } from "node:process";
+import "dotenv/config"; // oneliner for configuration
+import { closeDB, runDB } from "./db/database.js";
 
-const app = express()
-const port: number = Number(env.PORT) || 3000
-const address: string = "0.0.0.0" // Required for Render
+const app = express();
+const port: number = Number(env.PORT) || 3000;
+const address: string = "0.0.0.0"; // Required for Render
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+app.get("/user", (request, response) => {
+  const user: User = {
+    id: 1,
+    name: "David Karlsson",
+    email: "david@example.com",
+  };
+
+  response.status(201).send(user);
+});
 
 app.get("/", (request, response) => {
-    response.send("Hello World!")
-})
+  response.send("Hello World!");
+});
 
-app.listen(port, address, () => {
-console.log(`Listening to port ${port}`)
-})
+// Start server on Port variable
+
+async function startServer() {
+  try {
+    await runDB();
+    app.listen(port, address, () => {
+      console.log(`Listening to port ${port}`);
+      console.log(`Start the app: http://localhost:${port}`);
+    });
+    process.on("SIGINT", async () => {
+      console.log("Cleaning up...");
+      await closeDB();
+      process.exit(0);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+startServer();
